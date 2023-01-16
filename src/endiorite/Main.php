ca08ec2c2b7d17989b4d22176c2eaa31b74b94e0
JblusItsMe <jeanbaptiste.fourgeux@gmail.com>
2023-01-16 22:05:10 +0100

    Def variable

---
+++ src/endiorite/Main.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace endiorite;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase {
+
+    const PREFIX = "§l§9Endiorite §r§7»";
+    const MOTD = "§l§9Endiorite §f[1.19.50]";
+    const IP = "§9play.endiorite.com";
+    const SITE = "www.endiorite.fr";
+    const DISCORD = self::SITE . "/discord";
+    const VOTE = self::SITE . "/vote";
+
+    const DB_HOST = "endiorite.com";
+    const DB_USER = "u5_g2ySo67ymX";
+    const DB_PASS = "M646eXtbUZ83UP!w9bF^^=kt";
+    const DB_SESSION = "s5_Poney";
+    const DB_PORT = 3306;
+
+}
