647986deab252945469001ed98c8d9ef43d002a8
JblusItsMe <jeanbaptiste.fourgeux@gmail.com>
2023-01-17 19:26:11 +0100

    Loading your data

--- src/endiorite/Listener/PlayerManager.php
+++ src/endiorite/Listener/PlayerManager.php
@@ -4,10 +4,16 @@ namespace endiorite\Listener;
 
 use endiorite\Main;
 use endiorite\session\Account;
+use endiorite\session\Session;
+use pocketmine\data\bedrock\EffectIdMap;
+use pocketmine\data\bedrock\EffectIds;
+use pocketmine\entity\effect\EffectInstance;
+use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\item\ItemIds;
 use pocketmine\player\GameMode;
 use pocketmine\Server;
 
@@ -19,8 +25,17 @@ class PlayerManager implements Listener {
         $sender->teleport(Server::getInstance()->getWorldManager()->getDefaultWorld()->getSpawnLocation());
         $sender->setGamemode(GameMode::ADVENTURE());
 
+        if(!$sender->hasPlayedBefore()) {
+            $sender->sendMessage(Main::PREFIX . "§f Création de votre profil en cours...");
+            $account = new Account($sender);
+            $account->setup();
+            $sender->sendMessage(Main::PREFIX . "§f Votre profil à étais crée avec succès§!");
+        }
+        $sender->sendMessage(Main::PREFIX . "§f Chargement de vos donnée...");
         $account = new Account($sender);
-        $account->setup();
+        $account->updateDataOnJoin();
+        (new Session($sender));
+        $sender->sendMessage(Main::PREFIX . "§f Vos données sont chargées, bon jeu sur §l§6Endiorite§r§f.");
 
         $sender->getInventory()->clearAll();
         $sender->getArmorInventory()->clearAll();
@@ -38,6 +53,10 @@ class PlayerManager implements Listener {
         $sender = $event->getPlayer();
         $event->cancel();
         $sender->sendMessage(Main::PREFIX . "§c Le chat semble être désactivé");
+    }
+
+    public function onDamage(EntityDamageEvent $event) {
+        $event->cancel();
     }
 
 }
